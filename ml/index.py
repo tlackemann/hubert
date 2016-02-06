fa842b4143f73696a9102e8db62b0fd05aedd45e
Thomas Lackemann <tommylackemann@gmail.com>
2016-02-06 15:51:42 -0500

    some python cleanup

--- ml/index.py
+++ ml/index.py
@@ -24,6 +24,12 @@ for light in lights:
     # Now load all the events for this light
     light_events = session.execute('SELECT light_id, state_on, reachable, bri, hue, sat, x, y, ts FROM light_events WHERE light_id = %s ORDER BY ts DESC', [light.light_id])
     for event in light_events:
+        # Get the datetime of the event
+        event_time = cassandra.util.datetime_from_uuid1(event.ts)
+        # Monday is 0 and Sunday is 6
+        day_of_week = event_time.weekday()
+        current_hour = event_time.hour
+        current_minute = event_time.minute
         # Get the datetime of the timeuuid - We're going to convert this to a
         # "Week Minute" (0-10079 based scale based on how many minutes in a week)
         # 1440 = Minutes in a day
@@ -33,14 +39,9 @@ for light in lights:
         # Monday 1:00am = (0 * 1440) + (1 * 60) + 0 = 59
         # Tuesday 12:00am = (1 * 1440) + (0 * 60) + 0 = 1339
         # Sunday 11:59pm = (6 * 1440) + (23 * 60) + 59 = 8640 + 1380 + 59 = 10079
-        event_time = cassandra.util.datetime_from_uuid1(event.ts)
-        # Monday is 0 and Sunday is 6
-        day_of_week = event_time.weekday()
-        current_hour = event_time.hour
-        current_minute = event_time.minute
         week_hour = (day_of_week * 1440) + (current_hour * 60) + current_minute
         X.append([week_hour])
-
+        # Form the Z features
         event_state = 1 if (event.reachable and event.state_on) else 0
         Z.append([event_state, event.bri, event.hue, event.sat, event.x, event.y])
 
