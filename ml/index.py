020ad2026e5a843c341c2f0fcda137628b4df5a1
Thomas Lackemann <tommylackemann@gmail.com>
2016-02-06 20:13:09 -0500

    more ml linear regression
    - splits data into train/test
    - is kinder to smaller number of observations by including less features

--- ml/index.py
+++ ml/index.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 
+import time
 from time import sleep
 import numpy as np
 import cassandra
-from sklearn import linear_model
+from sklearn import linear_model, datasets
 from cassandra.cluster import Cluster
 
 print 'Initializing ...'
+start_time = time.time()
 
 print 'Connecting to Cassandra ...'
 # Connect to Cassandra
@@ -21,11 +23,13 @@ for light in lights:
     print 'Processing light "%s"' % light.name
     # Declare our X input (week hour)
     X = []
-    # Declare our Z output (reachable && state_on, bri, hue, sat, x, y)
-    Z = []
+    # Declare our Y output (reachable && state_on, bri, hue, sat, x, y)
+    Y = []
     # Now load all the events for this light
     sql = 'SELECT light_id, state_on, reachable, bri, hue, sat, x, y, ts FROM light_events WHERE light_id = %s ORDER BY ts DESC'
     light_events = session.execute(sql, [light.light_id])
+    total_rows = len(light_events.current_rows)
+    # Loop over each light and store the data in X and Y
     for event in light_events:
         # Get the datetime of the event
         event_time = cassandra.util.datetime_from_uuid1(event.ts)
@@ -44,20 +48,44 @@ for light in lights:
         # Sunday 11:59pm = (6 * 1440) + (23 * 60) + 59 = 8640 + 1380 + 59 = 10079
         week_hour = (day_of_week * 1440) + (current_hour * 60) + current_minute
         X.append([week_hour])
-        # Form the Z features
+        # Form the Y features
         event_state = 1 if (event.reachable and event.state_on) else 0
-        Z.append([event_state, event.bri, event.hue, event.sat, event.x, event.y])
+        # Based on our number of observations, we should be more kind to lesser
+        # observations to try and not overfit
+        if total_rows < 10000:
+            Y.append([event.hue, event.bri])
+        elif total_rows >= 10000 and total_rows < 50000:
+            Y.append([event.hue, event.bri, event.sat, event_state])
+        else:
+            print '@todo'
+            # @todo - Actually modify the light to what the best prediction is
+            Y.append([event.hue, event.bri, event.sat, event_state, event.x, event.y])
+
+    # Split the data into training/testing sets
+    X_train = X[:-20]
+    X_test = X[-20:]
+
+    # Split the targets into training/testing sets
+    Y_train = Y[:-20]
+    Y_test = Y[-20:]
+
     # Create a linear regression on the indv. light
     clf = linear_model.LinearRegression()
-    clf.fit(X, Z)
-    # Predict a custom time
-    # Sat 9:00pm = (5 * 1440) + (21 * 60) + 0 = 7200 + 1260 = 8460
-    print clf.predict(8460)
-    # Sun 9:00am = (6 * 1440) + (9 * 60) + 0 = 8640 + 540 = 9180
-    print clf.predict(9180)
-    # If we have over 50,000 rows, we can probably do something useful
-    if len(light_events.current_rows) >= 50000:
-        # @todo - Actually modify the light to what the best prediction is
-        print '@todo'
-    # Done!
-    print 'Done!'
+    clf.fit(X_train, Y_train)
+
+    # Print some useful information
+    if total_rows < 10000:
+        print 'Using features "hue" and "bri"'
+    elif total_rows >= 10000 and total_rows < 50000:
+        print 'Using features "hue", "bri", "sat", and "state_on"'
+    else:
+        print 'Using features "hue", "bri", "sat", "state_on", "x", and "y"'
+
+    print("Residual sum of squares: %.2f" % np.mean((clf.predict(X_test) - Y_test) ** 2))
+    print('Variance score: %.2f' % clf.score(X_test, Y_test)) # 1 is perfect prediction
+
+# Done!
+print 'Done!'
+end_time = time.time()
+total_time = end_time - start_time
+print 'Ran in %s seconds' % total_time
