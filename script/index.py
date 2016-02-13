604d78ed598ebb0373489814d1e7b5d24ef375cf
Thomas Lackemann <tommylackemann@gmail.com>
2016-02-13 11:25:56 -0500

    ml script sends all predicted states to rabbit

--- script/index.py
+++ script/index.py
@@ -174,16 +174,30 @@ for light in lights:
         if phase_1_condition:
             prediction_minutes = (right_now.hour * 60) + right_now.minute
             prediction = final_est.predict(prediction_minutes)
-            state_on_int = int(round(prediction[0][0]))
-            predict_state = 'ON' if state_on_int else 'OFF'
+            # Features: state, hue, bri, sat, x, y
             confidence = final_est.score(X_test, Y_test) # 1 is perfect prediction
-            state_message = json.dumps({ 'id': light.light_id, 'on': state_on_int })
+            predicted_state = {
+                'id': light.light_id,
+                'on': True if int(round(prediction[0][0])) == 1 else False,
+                'hue': int(prediction[0][1]),
+                'bri': int(prediction[0][2]),
+                'sat': int(prediction[0][3]),
+                'x': prediction[0][4],
+                'y': prediction[0][5]
+            }
+            state_message = json.dumps(predicted_state)
             rmq_channel.basic_publish(exchange='',routing_key=RABBITMQ_QUEUE,body=state_message)
             print '%.2f - "%s">> Modifying state of light ...' % (time.time(), light.name)
             print '%.2f - "%s">> The time is %s' % (time.time(), light.name, right_now)
-            print '%.2f - "%s">> Predicting current minute %s/%s' % (time.time(), light.name, prediction_minutes, minutes_in_day - 1)
-            print '%.2f - "%s">> Predicting state of light is: %s (Confidence: %.2f)' % (time.time(), light.name, predict_state, confidence)
-
+            print '%.2f - "%s">> Predicting for current minute %s/%s' % (time.time(), light.name, prediction_minutes, minutes_in_day - 1)
+            print '%.2f - "%s">> Predicting state: %s (Confidence: %.2f)' % (time.time(), light.name, 'ON' if predicted_state['on'] else 'OFF', confidence)
+            print '%.2f - "%s">> Predicting hue: %s (Confidence: %.2f)' % (time.time(), light.name, predicted_state['hue'], confidence)
+            print '%.2f - "%s">> Predicting bri: %s (Confidence: %.2f)' % (time.time(), light.name, predicted_state['bri'], confidence)
+            print '%.2f - "%s">> Predicting sat: %s (Confidence: %.2f)' % (time.time(), light.name, predicted_state['sat'], confidence)
+            print '%.2f - "%s">> Predicting x: %s (Confidence: %.2f)' % (time.time(), light.name, predicted_state['x'], confidence)
+            print '%.2f - "%s">> Predicting y: %s (Confidence: %.2f)' % (time.time(), light.name, predicted_state['y'], confidence)
+            # Update the state of our light
+            rmq_channel.basic_publish(exchange='', routing_key=RABBITMQ_QUEUE, body=state_message)
         elif phase_2_condition:
             print '@todo'
         else:
