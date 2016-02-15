057e49fa781681eb855d1366711221b45a76db57
Thomas Lackemann <tommylackemann@gmail.com>
2016-02-15 02:03:18 -0500

    WIP one-step installation
    `docker-compose.yml` will act as the development setup. For actual
    instances of Hubert, we'll be able to run `./bin/install` and then
    `./bin/hubert` to build all Docker containers, run the installation
    script, and start all containers.

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,12 @@ WORKDIR /usr/src/app
 
 RUN npm install nodemon -g
 
+COPY package.json /usr/src/app
+RUN npm install
+COPY src/ /usr/src/app/src
+COPY bin/ /usr/src/app/bin
+COPY config /usr/src/app/config
+COPY .babelrc /usr/src/app/
+RUN npm run build
+
 CMD [ "npm", "start" ]
--- src/install.js
+++ src/install.js
@@ -1,22 +1,38 @@
+import readline from 'readline'
 import hue from './hue'
 import Log from './log'
 
 // Setup logger
 const log = new Log('hubert-install')
 
+// Setup terminal input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 // Set the username
 const username = process.env.HUE_USER || 'hue-app-user';
 
+rl.question('Please press the "link" button on your Hue Bridge and then press any key to continue', (answer) => {
+  hue.getDefaultBridge()
+    .then((bridge) => {
+      const user = hue.registerUser(
+        bridge.ipaddress,
+        username
+      )
+      return user
+    })
+    .then((user) => {
+      log.info('User created: %s (%s)', username, user)
+      log.info('Done!')
+      rl.close();
+    })
+    .catch((err) => {
+      log.error("There was a problem creating the user: %s", err)
+      process.exit(1)
+      rl.close();
+    })
+});
+
 // Create the user
-hue.getDefaultBridge()
-  .then((bridge) => {
-    const user = hue.registerUser(
-      bridge.ipaddress,
-      username
-    )
-    return user
-  })
-  .then((user) => {
-    log.info('User created: %s (%s)', username, user)
-    log.info('Done!')
-  })
